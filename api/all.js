96a86da5f8010950f45e2aef73e6e6aa59e06c12
AmHustler <haripavanveera@gmail.com>
2026-03-01 18:58:33 +0530

    Update all.js

--- api/all.js
+++ api/all.js
@@ -1,8 +1,17 @@
 export default async function handler(req, res) {
-  const response = await fetch(
-    "https://monxcxbrwkjaiwaiosdg.supabase.co/functions/v1/get_all_yt_snapshots"
-  );
+  const url =
+    "https://monxcxbrwkjaiwaiosdg.supabase.co/rest/v1/snapshots_meta?select=*&order=snapshot_time.desc&limit=48";
 
-  const data = await response.json();
-  res.status(200).json(data);
+  try {
+    const response = await fetch(url, {
+      headers: {
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vbnhjeGJyd2tqYWl3YWlvc2RnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1NzgwNzUsImV4cCI6MjA4NTE1NDA3NX0.xHJNYLKKzhuMmHuJvm_agIKQq5puEeuyQlQCgxmJMmI",
+      },
+    });
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Proxy failed" });
+  }
 }
